a527ed4a41b219b424cc03880875ecf343dd56a2
Sabu Siyad <hello@ssiyad.com>
2025-02-15 22:04:30 +0530

    feat: document resource

---
+++ document-resource/index.ts
@@ -0,0 +1,63 @@
+import { api } from '../api';
+import { Resource } from '../resource';
+
+export class DocumentResource extends Resource {
+  /**
+   * Save this document. If successful, refresh the document.
+   */
+  async save() {
+    return api({
+      url: 'frappe.desk.form.save.savedocs',
+      method: 'post',
+      body: {
+        doc: JSON.stringify(this.data),
+        action: 'Save',
+      },
+    }).then(() => this.refresh());
+  }
+
+  /**
+   * Submit this document. If successful, refresh the document.
+   */
+  async submit() {
+    return api({
+      url: 'frappe.desk.form.save.savedocs',
+      method: 'post',
+      body: {
+        doc: JSON.stringify(this.data),
+        action: 'Submit',
+      },
+    }).then(() => this.refresh());
+  }
+
+  /**
+   * Cancel this document. If successful, refresh the document.
+   */
+  async cancel() {
+    return api({
+      url: 'frappe.desk.form.save.savedocs',
+      method: 'post',
+      body: {
+        doc: JSON.stringify(this.data),
+        action: 'Cancel',
+      },
+    }).then(() => this.refresh());
+  }
+}
+
+/**
+ * Create a `DocumentResource`.
+ * @param doctype - Document type.
+ * @param docname - Document name.
+ * @returns `DocumentResource`
+ */
+export function createDocumentResource({
+  doctype,
+  docname,
+}: {
+  doctype: string;
+  docname: string;
+}) {
+  const url = '/api/resource/' + doctype + '/' + docname;
+  return new DocumentResource(url);
+}
--- resource/index.ts
+++ resource/index.ts
@@ -1,6 +1,6 @@
 import { api } from '../api';
 
-class Resource {
+export class Resource {
   data: any;
 
   constructor(
