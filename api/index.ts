d0f8ff96e8a0708a9fe8ee9f853cf820a5622a01
Sabu Siyad <hello@ssiyad.com>
2025-02-19 00:03:26 +0530

    feat: cache for `api`

--- api/index.ts
+++ api/index.ts
@@ -6,24 +6,43 @@ import { straw } from '../shared';
  * @param method - HTTP method to use.
  * @param body - Data to send in request.
  * @param params - Query parameters to send in request.
+ * @param cache - Whether to cache the response.
  * @returns Promise
  */
-export const api = ({
+export const api = async ({
   url,
   method,
   params,
   body,
+  cache,
 }: {
   url: string;
   method: 'get' | 'post' | 'put' | 'delete' | 'patch' | 'options';
   body?: Record<string, any>;
   params?: Record<string, any>;
+  cache?: boolean;
 }) => {
-  return straw.client.request({
-    baseURL: url.startsWith('http') ? '' : undefined,
-    url,
-    method,
-    params,
-    data: body,
-  });
+  if (cache) {
+    const key = JSON.stringify({ url, method, body, params });
+    const cached = straw.cache.get(key);
+    if (cached) {
+      return Promise.resolve(cached);
+    }
+  }
+
+  return straw.client
+    .request({
+      baseURL: url.startsWith('http') ? '' : undefined,
+      url,
+      method,
+      params,
+      data: body,
+    })
+    .then(({ data }) => {
+      if (cache) {
+        const key = JSON.stringify({ url, method, body, params });
+        straw.cache.set(key, data);
+      }
+      return data;
+    });
 };
--- shared.ts
+++ shared.ts
@@ -1,9 +1,15 @@
+import TTLCache from '@isaacs/ttlcache';
 import { AxiosInstance } from 'axios';
 
 type Straw = {
   client: AxiosInstance;
+  cache: TTLCache<unknown, unknown>;
 };
 
 export const straw: Straw = {
   client: {} as AxiosInstance,
+  cache: new TTLCache({
+    max: 1000,
+    ttl: 1000 * 60 * 5,
+  }),
 };
