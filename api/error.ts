b7f05dca648c2383b5210a35f7ceef9d54a50ca7
Sabu Siyad <hello@ssiyad.com>
2025-04-22 21:40:27 +0530

    fix: get error message from response if exists
    closes https://github.com/ssiyad/frappe-straw/issues/31

--- api/error.ts
+++ api/error.ts
@@ -10,10 +10,12 @@ export const toStrawError = ({
   // Check `ServerMessage` type for more details.
   const serverMessagesStr = (response?.data as any)._server_messages || '[]';
 
+  // Parse server messages string to JSON.
   const err: ServerMessage = JSON.parse(
     JSON.parse(serverMessagesStr).at(0) ?? null,
   );
 
+  // If the error is a server message, return it.
   if (err) {
     return {
       ...err,
@@ -22,6 +24,17 @@ export const toStrawError = ({
     };
   }
 
+  // If the error is a validation error, return it.
+  // @ts-ignore `message` may exist on the response object.
+  if (status > 400 && response?.data?.message) {
+    return {
+      // @ts-ignore `message` may exist on the response object.
+      message: response.data.message,
+      status,
+    };
+  }
+
+  // If the error is a network error, return it.
   return {
     message,
     status,
